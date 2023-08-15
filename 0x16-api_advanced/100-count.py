87248947c787f9f5c478e99058f341de099d9937
bwalya-305 <bwalyamwansa305@gmail.com>
2023-08-15 15:16:56 -0700

    recursive function that queries the Reddit API

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -5,15 +5,15 @@ of given keywords (case-insensitive, delimited by spaces.
 Javascript should count as javascript, but java should not).
 """
 import pprint
-import re
+import sys
 import requests
 
-BASE_URL = 'https://www.reddit.com/r/{subreddit}/hot.json'
+BASE_URL = 'https://reddit.com/r/{}/hot.json'
 
 
 def count_words(subreddit, word_list, hot_list=[], after=None):
     '''function count_words : Get ALL hot posts'''
-    headers = {'User-agent': 'Mozilla/5.0'}
+    headers = {'User-agent': 'Unix:0-subs:v1'}
     params = {'limit': 100}
     if isinstance(after, str):
         if after != "STOP":
@@ -42,7 +42,7 @@ def print_results(word_list, hot_list):
     for title in hot_list:
         for word in word_list:
             count[word] = count[word] +\
-             len(re.findall(r'\b{}\b'.format(word), title, re.I))
+             len(re.findall(r'(?:^| ){}(?:$| )'.format(word), title, re.I))
 
     count = {k: v for k, v in count.items() if v > 0}
     words = sorted(list(count.keys()))
